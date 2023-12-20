0e2160d3cffff73bc06f2b5a18f8a26943f90fc2
cfeliciano7792 <cfeliciano7792@gmail.com>
2023-12-20 11:57:46 -0500

    Add "grass" background

--- main.py
+++ main.py
@@ -155,6 +155,7 @@ class Main:
         self.check_fail()
 
     def draw_elements(self):
+        self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
 
@@ -181,9 +182,17 @@ class Main:
 
     def draw_grass(self):
         grass_color = (167, 209, 61)
-
-        for col in range(cell_number):
-            grass_rect = pygame.Rect(col * cell_size, 0, cell_size, cell_size)
+        for row in range(cell_number):
+            if row % 2 == 0:
+                for col in range(cell_number):
+                    if col % 2 == 0:
+                        grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+            else:
+                for col in range(cell_number):
+                    if col % 2 != 0:
+                        grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
 
 
 pygame.init()
